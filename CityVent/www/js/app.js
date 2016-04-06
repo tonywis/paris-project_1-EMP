6bc4615484d745b1eeb7f3aeb1a13041dd79b6df
Tony Wisniewski <twisniewski@keley-live.com>
2016-04-06 15:52:34 +0200

    add ngCordova for angular

--- CityVent/www/js/app.js
+++ CityVent/www/js/app.js
@@ -4,7 +4,7 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 
-angular.module('app', ['ionic','ionic-material'])
+angular.module('app', ['ionic','ionic-material', 'ngCordova'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
