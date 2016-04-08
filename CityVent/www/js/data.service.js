1884228c3e6f4a9519591ed8e73e5b7f7a1cd8d3
Tony Wisniewski <twisniewski@keley-live.com>
2016-04-08 15:50:08 +0200

    add object request & fix bug load file & remove useless file

--- CityVent/www/js/data.service.js
+++ CityVent/www/js/data.service.js
@@ -1,6 +1,14 @@
 angular.module("app")
-	.service('dataService', function(quefaire) {
+	.service('dataService', function(QueFaireService) {
 		this.data = {};
+		this.request = {
+			"restaurant": 0,
+			"bar": 0,
+			"club": 0,
+			"spectacle": 0,
+			"concert": 0,
+			"random": 0
+		};
 
 
 		this.format_APIGoogle = function(response) {}
