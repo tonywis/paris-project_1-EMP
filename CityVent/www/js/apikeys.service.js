bea5866e50e514631ccac649d13da892b9c0aa10
Tony Wisniewski <twisniewski@keley-live.com>
2016-04-21 22:43:41 +0200

    Export and merge APIkeys in service

---
+++ CityVent/www/js/apikeys.service.js
@@ -0,0 +1,5 @@
+angular.module('app')
+.service('APIKeys', function(){
+	this.PARIS_TOKEN = "9374c2e664f094392c08daf3355c48e647b3653a9cb88cb97becb20a14838028";
+	this.GETTY_IMG_TOKEN = "q5huf6j8bg8qt3jnmxd536v7";
+})
