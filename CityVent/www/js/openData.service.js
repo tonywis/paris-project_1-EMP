df7ff2b50cd06b0740464e7867d5b5d7879bf70c
Tony Wisniewski <twisniewski@keley-live.com>
2016-04-24 21:21:14 +0200

    add filter in openDataService & change format answer

--- CityVent/www/js/openData.service.js
+++ CityVent/www/js/openData.service.js
@@ -1,44 +1,67 @@
 angular.module("app")
-	.service('openDataService', function($http,$filter){
+	.service('openDataService', function($http){
 		
-        var resultTab = [];
-    
-        function transformResult(result){
-            var randInt = Math.floor(Math.random()*100);
+        function transformResult(results, nb){
+            //console.log("format "+results);
+            if(results.length == 0)
+                return [];
+            nb = results.length < nb ? results.length : nb;
+            var formated = [];
+            for(var i=0; i<nb; i++) {
+                var randInt = Math.floor(Math.random()*results.length);
 
-            var dataChoosed= result.records[randInt].fields;
-
-            return {
-                "address": dataChoosed.address,
-                "date_end": dataChoosed.date_end,
-                "date_start": dataChoosed.date_start,
-                "description": dataChoosed.description,
-                "free_text": dataChoosed.free_text,
-                "image": dataChoosed.image,
-                "image_thumb": dataChoosed.image_thumb,
-                "placename": dataChoosed.placename,
-                "pricing": dataChoosed.pricing_info,
-                "link": dataChoosed.link,
-                "title": dataChoosed.title,
-                "open": dataChoosed.date_start,
-                "end": dataChoosed.date_end
+                var dataChoosed= results[randInt].fields;
+                var object = {
+                    "address": dataChoosed.address,
+                    "place_name": dataChoosed.placename,
+                    "name": dataChoosed.title,
+                    "small_description": dataChoosed.description,
+                    "description": dataChoosed.free_text,
+                    "image_thumb": dataChoosed.image_thumb,
+                    "image": dataChoosed.image,
+                    "open": dataChoosed.date_start,
+                    "end": dataChoosed.date_end,
+                    "price": dataChoosed.pricing_info,
+                    "link": dataChoosed.link
+                };
+                formated.push(object);
+                results.splice(randInt, 1);
             }
+            console.log(formated);
+            return formated;
+        }
+
+        function only_open(results) {
+            return results.records.filter(function(d) {
+                var dateStart = new Date(d.fields.date_start);
+                var dateEnd = new Date(d.fields.date_end);
+                var dateNow = new Date();
+                /*console.log("start : "+dateStart.toDateString());
+                console.log("end : "+dateEnd.toDateString());
+                console.log("now :" +dateNow.toDateString());*/
+                if(dateEnd <= dateNow)
+                    return false;
+                else if(dateStart <= dateNow || (dateStart.getDay() == dateNow.getDay() && dateStart.getDate() == dateNow.getDate() && dateStart.getFullYear() == dateNow.getFullYear()))
+                    return true;
+                else
+                    return false;
+            });
         }
     
         this.get_clubs = function(num) {
-            return $http.get("http://opendata.paris.fr//api/records/1.0/search/?dataset=evenements-a-paris&rows=100&refine.tags=clubbing")
+            return $http.get("http://opendata.paris.fr//api/records/1.0/search/?dataset=evenements-a-paris&sort=date_start&rows=100&refine.tags=clubbing")
             .then(function success(results){
                 console.log(results);
-                return transformResult(results.data);
+                return transformResult(only_open(results.data), num);
             },function (error){
                 return error;
             });
         };
     
         this.get_concerts = function(num) {
-            return $http.get("http://opendata.paris.fr//api/records/1.0/search/?dataset=cinemas-a-paris&rows=100&refine.tags=concert")
+            return $http.get("http://opendata.paris.fr//api/records/1.0/search/?dataset=cinemas-a-paris&sort=date_start&rows=100&refine.tags=concert")
             .then(function success(results){
-                return results.data;
+                return transformResult(only_open(results.data), num);
             },function (error){
                 return error;
             });
