1b7f49866928920bfbf0f2d016fced8d82586850
PaulOurso <david_paul2@hotmail.fr>
2016-04-27 00:40:37 +0200

    develop multiple data request for googleAPI + refactor and comment openData and googleAPI

--- CityVent/www/js/googlePlaces.service.js
+++ CityVent/www/js/googlePlaces.service.js
@@ -1,30 +1,42 @@
 angular.module('app')
 .service('googlePlacesService', function($http,APIKeys) {
     
-	this.get_restaurant = function(){
-        return text_search("restaurant");
+    //request to get n*nb restaurants for today
+	this.get_restaurant = function(nb){
+        return text_search("restaurant",nb);
     }
     
-    this.get_bar = function(){
-        return text_search("bar");
+    //request to get n*nb bars for today
+    this.get_bar = function(nb){
+        return text_search("bar",nb);
     }
     
-    function text_search(place) {
+    //request to get n*nb places for today
+    function text_search(place,nb) {
         
 		return $http.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+place+"+in+Paris&key="+APIKeys.GOOGLEPLACES_ANDROID_TOKEN)
             .then(function success(results){
-                return transformResult(only_open(results.data.results));
+                return transformResult(only_open(results.data.results),nb);
             },function (error){
                 return error;
             });
 	}
     
-    function transformResult(results){
+    //function to select randomly n*nb informations in all results from the request
+    function transformResult(results,nb){
+        
+        var formated = [];
+        
+        //if no results, return nothing
+        if(results.length == 0)
+            return formated;
+            
+        //take the minimun between the numbers of data and the numbers asked
+        nb = results.length < nb ? results.length : nb;
+        for(var i=0; i<nb; i++) {
             var randInt = Math.floor(Math.random()*results.length);
-
             var dataChoosed= results[randInt];
-
-            return {
+            var randObject=  {
                 "address": dataChoosed.formatted_address,
                 "place_name": dataChoosed.name,
                 "name": null,
@@ -37,8 +49,12 @@ angular.module('app')
                 "open": null,
                 "end": null,
             };
+            formated.push(randObject);
+        }
+        return formated;
     }
     
+    //filter all places who are not open today
     function only_open(results) {
         return results.filter(function(d) {
             if(d.opening_hours)
