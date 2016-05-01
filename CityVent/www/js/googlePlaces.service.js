8595c60da643d5a6afbd0570d5a1d946b3de6bd1
Tony Wisniewski <twisniewski@keley-live.com>
2016-05-01 16:46:10 +0200

    Update request googleAPI with location

--- CityVent/www/js/googlePlaces.service.js
+++ CityVent/www/js/googlePlaces.service.js
@@ -1,21 +1,26 @@
 angular.module('app')
-.service('googlePlacesService', function($http,APIKeys) {
+.service('googlePlacesService', function($http,APIKeys, locationService) {
     
     //request to get n*nb restaurants for today
 	this.get_restaurant = function(nb, callbackDataService){
-        text_search("restaurant",nb, callbackDataService);
+        text_search("restaurant|food",nb, callbackDataService);
     }
     
     //request to get n*nb bars for today
     this.get_bar = function(nb, callbackDataService){
-        text_search("bar",nb, callbackDataService);
+        text_search("bar|cafe",nb, callbackDataService);
     }
     
     //request to get n*nb places for today
-    function text_search(place,nb, callbackDataService) {
-		$http.get("https://maps.googleapis.com/maps/api/place/textsearch/json?query="+place+"+in+Paris&key="+APIKeys.GOOGLEPLACES_ANDROID_TOKEN)
+    function text_search(place, nb, callbackDataService) {
+        var url;
+        if(locationService.position.lat != null && locationService.position.lng != null)
+            url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+locationService.position.lat+","+locationService.position.lng+"&radius=3000&types="+place+"&opennow=true&rankby=prominence&key="+APIKeys.GOOGLEPLACES_ANDROID_TOKEN;
+        else
+            url = "https://maps.googleapis.com/maps/api/place/textsearch/json?query="+place+"+in+Paris&opennow=true&key="+APIKeys.GOOGLEPLACES_ANDROID_TOKEN;
+		$http.get(url)
             .then(function (results){
-                callbackDataService(transformResult(only_open(results.data.results),nb));
+                callbackDataService(transformResult(results.data.results,nb));
             },function (error){
                 callbackDataService([]);
             });
@@ -52,14 +57,4 @@ angular.module('app')
         }
         return formated;
     }
-    
-    //filter all places who are not open today
-    function only_open(results) {
-        return results.filter(function(d) {
-            if(d.opening_hours)
-                return d.opening_hours.open_now;
-            else
-                return true;
-        });
-    }  
 });
