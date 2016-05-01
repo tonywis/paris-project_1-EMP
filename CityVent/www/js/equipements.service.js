98affa4af1a80119fa4d3e823f47e12042832eee
Tony Wisniewski <twisniewski@keley-live.com>
2016-05-01 21:11:26 +0200

    remove unless file js

--- CityVent/www/js/equipements.service.js
+++
@@ -1,7 +0,0 @@
-angular.module('app')
-.service('Equipement', function($http, APIParisToken) {
-    
-	this.get_categories = function() {
-		return $http("https://api.paris.fr/api/data/1.0/Equipements/get_categories/?token="+APIParisToken.token);
-	}
-});
